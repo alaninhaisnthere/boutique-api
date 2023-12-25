e2199b2d2a3d7b4e4e227f5ab025a645f6716d92
Alana Oliveira <alanaoliveira71@gmail.com>
2023-12-25 09:53:02 -0300

    fix: Fixing commit bug'

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -13,7 +13,7 @@ async function getAllProducts() {
 async function getProductById(productId) {
     const connection = await pool.getConnection();
     try {
-        const [rows] = await connection.query('SELECT * FROM produtos WHERE id = ?', [productId]);
+        const [rows] = await connection.query('SELECT * FROM produts WHERE id = ?', [productId]);
         return rows[0];
     } finally {
         connection.release();
