d18ed3adcee2c6cae25c1a7fa115d3cef81d04ee
Alana Oliveira <alanaoliveira71@gmail.com>
2023-12-28 12:45:40 -0300

    feat: Implement SQL database integration for user registration

--- src/config/database.js
+++ src/config/database.js
@@ -7,8 +7,8 @@ function createConnectionPool() {
         port: process.env.DB_PORT,
         database: process.env.DB_DATABASE,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD
+        password: process.env.DB_PASSWORD,
     });
 }
 
-module.exports = createConnectionPool;
+module.exports = createConnectionPool();
---
+++ src/controllers/userController.js
@@ -0,0 +1,31 @@
+const pool = require('../config/database');
+
+async function registerUser(req, res) {
+    const { username, email, password } = req.body;
+
+    console.log('Request Body:', req.body);
+
+    if (!username || !email || !password) {
+        console.log('Invalid request parameters');
+        return res.status(400).json({ error: "Username, email, and password are required" });
+    }
+
+    console.log(pool);
+
+    const connection = await pool.getConnection();
+    try {
+        const [result] = await connection.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, password]);
+        console.log('User registered successfully:', result.insertId);
+        res.json({ userId: result.insertId, message: "User registered successfully" });
+    } catch (error) {
+        console.error('Error during user registration:', error);
+        res.status(500).json({ error: "Internal Server Error" });
+    } finally {
+        connection.release();
+    }
+}
+
+
+module.exports = {
+    registerUser,
+};
