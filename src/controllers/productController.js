37b23be985753fe04cc7a88bee943489fabd373d
Alana Oliveira <alanaoliveira71@gmail.com>
2023-12-28 00:51:05 -0300

    fix: Remove duplicate import of productModel

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,48 +1,48 @@
-const productModel = require('../models/productModel');
+const productModel = require("../models/productModel");
 
 async function getAllProducts(req, res) {
-    try {
-        const products = await productModel.getAllProducts();
-        res.json(products);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const products = await productModel.getAllProducts();
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 async function getProductById(req, res) {
-    try {
-        const product = await productModel.getProductById(req.params.productId);
-        if (!product) {
-            return res.status(404).json({ error: 'Product not found' });
-        }
-        res.json(product);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const product = await productModel.getProductById(req.params.productId);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
     }
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
-const productModel = require('../models/productModel');
-
 async function getProductsByCategory(req, res) {
-    try {
-        const category = req.params.category;
-        const products = await productModel.getProductsByCategory(category);
+  try {
+    const category = req.params.category;
+    const products = await productModel.getProductsByCategory(category);
 
-        if (products.length === 0) {
-            return res.status(404).json({ error: 'No products found in the specified category' });
-        }
-
-        res.json(products);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (products.length === 0) {
+      return res
+        .status(404)
+        .json({ error: "No products found in the specified category" });
     }
+
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 module.exports = {
-    getAllProducts,
-    getProductById,
-    getProductsByCategory
+  getAllProducts,
+  getProductById,
+  getProductsByCategory,
 };
