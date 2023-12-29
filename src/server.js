9ada8578077439d8dd4d8195a1dd046ede1417ae
Alana Oliveira <alanaoliveira71@gmail.com>
2023-12-29 07:40:18 -0300

    fix: Updating homeRoutes bug

--- src/server.js
+++ src/server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const productRoutes = require("./routes/productRoutes");
+const homeRoutes = require("./routes/homeRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 app.use(cors());
+
 app.use(express.json());
 
-app.use("/api/products", productRoutes);
+app.use("/api", productRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/", homeRoutes);
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
